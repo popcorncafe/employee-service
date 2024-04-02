9997acf8611923ff7fba9ddd84c8004ec22001d3
Artyom Nefedov <74130706+artemnefedov@users.noreply.github.com>
2024-04-02 13:34:54 +0400

    up char limit for emp name, add order by surname for page req

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,7 +1,7 @@
 CREATE TABLE position
 (
     id     uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    name   varchar(20) NOT NULL,
+    name   varchar(30) NOT NULL,
     salary float4      NOT NULL DEFAULT 0,
     roles  smallint[]  NOT NULL
 );
@@ -9,10 +9,10 @@ CREATE TABLE position
 CREATE TABLE employee
 (
     id                  uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    firstname           varchar(20) NOT NULL,
-    surname             varchar(20) NOT NULL,
+    firstname           varchar(40) NOT NULL,
+    surname             varchar(40) NOT NULL,
     date_of_birth       date        NOT NULL,
-    email               varchar(20) NULL UNIQUE,
+    email               varchar(40) NULL UNIQUE,
     phone_number        varchar(20) NULL UNIQUE,
     register_date       timestamptz NOT NULL DEFAULT current_timestamp,
     position_id         uuid        NULL REFERENCES position (id),
